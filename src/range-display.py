3d607cc47094317817d08a81252053a65339155a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-09-14 08:46:15 -0400

    Add the starting version of printing range display program in src.

---
+++ src/range-display.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+print()
+
+for i in range(20):
+    print("2 to the " + str(i)
+          + " power is " + str(2**i))
+
+print()
+
+i = 0
+while i < 20:
+    print("2 to the " + str(i)
+          + " power is " + str(2**i))
+    i += 1
