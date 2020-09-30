2c072eb4f49078dfcad17d3bcb314c9cd5baf1c6
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-09-30 10:03:31 -0400

    Add all of the debugging statements to the mean compute program.

--- src/mean-compute.py
+++ src/mean-compute.py
@@ -1,11 +1,18 @@
 def compute_mean(numbers):
+    print(f"Called compute_mean with numbers = {numbers}")
     s = sum(numbers)
+    print(f"Sum of numbers = {s}")
     N = len(numbers)
+    print(f"Length of numbers = {N}")
     mean = s / N
+    print(f"Calculated mean = {mean}")
     return mean
 
 
 numbers = [5,10,5,10,5]
+print(str(compute_mean(numbers)))
+
+numbers = [5,1,7,99,4]
 print(str(compute_mean(numbers)))
 
 
