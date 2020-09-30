3618a227ffa99b4935a34e29c50135df34aff183
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-09-30 09:31:05 -0400

    Add debugging statements to the factorial function.

--- src/factorial-function.py
+++ src/factorial-function.py
@@ -1,8 +1,11 @@
 #!/bin/python3
 
 def factorial(number: int):
+    print(f"Current call factorial({number})")
     if number == 1:
+        print("  ... Hit the base case!")
         return 1
+    print(f"  ... Now calling factorial({number-1})")
     return number * factorial(number - 1)
 
 num = 5
