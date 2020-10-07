cf9944d493e5779d2723684933677f07d0e0f101
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-10-07 10:10:03 -0400

    Add more code to the string concatenation example.

--- src/string-concatenation.py
+++ src/string-concatenation.py
@@ -1,9 +1,16 @@
 hello = "hello"
 world = "world"
 space = " "
+empty = ""
 
 message = hello + space + world
 
 print("First example of string concatenation 😃")
+print(f"The message is: {message}")
+print("")
+
+message = world + space + hello
+
+print("Second example of string concatenation 😲")
 print(f"The message is: {message}")
 print("")
