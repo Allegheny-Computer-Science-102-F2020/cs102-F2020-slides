0d6cc23e1ea817c81b42f35937fc90b2e27491be
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-10-26 06:53:56 -0400

    Add the set-set-creation-examples.py file in the src directory.

---
+++ src/set-creation-examples.py
@@ -0,0 +1,13 @@
+# define a set using a list
+x = set(['pencil', 'paper', 'pen', 'pencil', 'wallet', 'pen'])
+
+print("Set defined with a list:")
+print(x)
+
+print()
+
+# define a set using a tuple
+x = set(('pencil', 'paper', 'pen', 'pencil', 'wallet', 'pen'))
+
+print("Set defined with a tuple:")
+print(x)
