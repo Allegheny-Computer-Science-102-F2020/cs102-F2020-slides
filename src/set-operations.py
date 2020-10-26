9ea2555cf2e06b1c765ab6930c8f5a41272ab64b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-10-26 08:11:27 -0400

    Add the set-operations.py file in the src directory for week nine.

---
+++ src/set-operations.py
@@ -0,0 +1,20 @@
+# Reference:
+# https://realpython.com/python-sets/
+
+# define the sets
+a = {1, 2, 3, 4}
+b = {2, 3, 4, 5}
+c = {3, 4, 5, 6}
+d = {4, 5, 6, 7}
+
+# union
+print("Union of the sets:")
+print(a.union(b, c, d))
+print(a | b | c | d)
+
+print()
+
+# intersection
+print("Intersection of the sets:")
+print(a.intersection(b, c, d))
+print(a & b & c & d)
