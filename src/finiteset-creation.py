2059b3940d97e183b90f44daedc14c6909eea7f6
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-11-02 07:22:20 -0500

    Add more examples with the tuple creation in finiteset-creation.py.

--- src/finiteset-creation.py
+++ src/finiteset-creation.py
@@ -2,22 +2,33 @@ from sympy import FiniteSet
 
 # create a finite set
 finite_set = FiniteSet(2, 4, 6, 8, 10)
+print("Explicit FiniteSet:")
 print(finite_set)
 print()
 
 # create an empty set
 empty_set = FiniteSet()
+print("Empty FiniteSet:")
 print(empty_set)
 print()
 
 # create a set from a list
 list = [2, 4, 6, 8, 10]
 finite_set = FiniteSet(*list)
+print("FiniteSet from List:")
 print(finite_set)
 print()
 
 # create a set from a tuple
 tuple = (2, 4, 6, 8, 10)
+print("FiniteSet from Tuple:")
 finite_set = FiniteSet(*tuple)
+print(finite_set)
+print()
+
+# create a set containing a tuple
+tuple = (2, 4, 6, 8, 10)
+print("FiniteSet Containing Tuple:")
+finite_set = FiniteSet(tuple)
 print(finite_set)
 print()
