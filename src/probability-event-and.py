8f501265d44428fb2a5de9a25bd244c9f9b0196f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-11-02 08:50:43 -0500

    Add the two final programs about probability and events.

---
+++ src/probability-event-and.py
@@ -0,0 +1,9 @@
+from sympy import FiniteSet
+
+six_sided = FiniteSet(1, 2, 3, 4, 5, 6)
+roll_one = FiniteSet(2, 3, 5)
+roll_two = FiniteSet(1, 3, 5)
+
+event = roll_one.intersect(roll_two)
+prob = len(event) / len(six_sided)
+print(prob)
