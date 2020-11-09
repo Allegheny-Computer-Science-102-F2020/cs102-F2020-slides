8c09adf09d18eb10f61ac015f37f5cf7637bc7e0
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-11-09 08:20:16 -0500

    Add the starting version of the dictionary-example.py program in src.

---
+++ src/dictionary-example.py
@@ -0,0 +1,32 @@
+# create the dictionary
+
+MLB_team = dict(
+    Colorado='Rockies',
+    Boston='Red Sox',
+    Minnesota='Twins',
+    Milwaukee='Brewers',
+    Seattle='Mariners'
+)
+
+# determine details about the dictionary
+
+print(type(MLB_team))
+print(MLB_team)
+
+# access content that exists in the dictionary
+
+print(MLB_team['Minnesota'])
+
+print(MLB_team['Colorado'])
+
+# add new content to the dictionary
+
+MLB_team['Kansas City'] = 'Royals'
+
+# determine details about the dictionary
+
+type(MLB_team)
+MLB_team
+# access content that does not exist in the dictionary
+
+print(MLB_team['Toronto'])
