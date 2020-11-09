0d1b17cfde15d4abea8898cae29df2baea718f72
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-11-09 09:04:14 -0500

    Fix the capitalization in the dictionary-example.py file.

--- src/dictionary-example.py
+++ src/dictionary-example.py
@@ -1,6 +1,6 @@
 # create the dictionary
 
-MLB_team = dict(
+mlb_team = dict(
     Colorado='Rockies',
     Boston='Red Sox',
     Minnesota='Twins',
@@ -10,23 +10,23 @@ MLB_team = dict(
 
 # determine details about the dictionary
 
-print(type(MLB_team))
-print(MLB_team)
+print(type(mlb_team))
+print(mlb_team)
 
 # access content that exists in the dictionary
 
-print(MLB_team['Minnesota'])
-print(MLB_team['Colorado'])
+print(mlb_team['Minnesota'])
+print(mlb_team['Colorado'])
 
 # add new content to the dictionary
 
-MLB_team['Kansas City'] = 'Royals'
+mlb_team['Kansas City'] = 'Royals'
 
 # determine details about the dictionary
 
-print(type(MLB_team))
-print(MLB_team)
+print(type(mlb_team))
+print(mlb_team)
 
 # access content that does not exist in the dictionary
 
-print(MLB_team['Toronto'])
+print(mlb_team['Toronto'])
