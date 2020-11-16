e9c80c419f2349eff8dbdb569538f8d066159e16
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-11-16 09:57:26 -0500

    Add the dog-dictionary.py program in the src directory.

---
+++ src/dog-dictionary.py
@@ -0,0 +1,31 @@
+# define the characteristics of a dog using a dictionary
+
+bosco = {}
+
+bosco["Name"] = "Bosco"
+bosco["Age"] = 6
+bosco["Breed"] = "Havanese"
+
+faith = {}
+
+faith["Name"] = "Faith"
+faith["Age"] = 14
+faith["Breed"] = "Havanese"
+
+print()
+
+# display the characteristics of the dog named Bosco
+
+print(f"The name of the dog is: {bosco['Name']}")
+print(f"The age of the dog is: {bosco['Age']}")
+print(f"The breed of the dog is: {bosco['Breed']}")
+
+print()
+
+# display the characteristics of the dog named Faith
+
+print(f"The name of the dog is: {faith['Name']}")
+print(f"The age of the dog is: {faith['Age']}")
+print(f"The breed of the dog is: {faith['Breed']}")
+
+print()
