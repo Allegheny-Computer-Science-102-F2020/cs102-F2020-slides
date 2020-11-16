769a6e65b21cc0226e430afc048491dcb12e9852
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-11-16 10:19:56 -0500

    Add the remaining source code for the dog-object.py program.

--- src/dog-object.py
+++ src/dog-object.py
@@ -10,5 +10,27 @@ class Dog:
 # create an instance of the Dog class for Bosco
 bosco = Dog("Bosco", 6, "Havanese")
 
+# create an instance of the Dog class for Faith
+faith = Dog("Faith", 14, "Havanese")
+
 # display the address of the Bosco instance
 print(bosco)
+
+print()
+
+print(f"The dog's name is: {bosco.name}")
+print(f"The dog's age is: {bosco.age}")
+print(f"The dog's breed is: {bosco.breed}")
+
+print()
+
+# display the address of the Faith instance
+print(faith)
+
+print()
+
+print(f"The dog's name is: {faith.name}")
+print(f"The dog's age is: {faith.age}")
+print(f"The dog's breed is: {faith.breed}")
+
+print()
