cd93c0633d27e74daf86bfc05cd721c5604331b2
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-11-16 09:27:32 -0500

    Add the dog-list.py showing how to have "objects" as a list!

---
+++ src/dog-list.py
@@ -0,0 +1,22 @@
+# define the characteristics of a dog using a list
+
+bosco = ["Bosco", 6, "Havanese"]
+faith = ["Faith", 14, "Havanese"]
+
+print()
+
+# display the characteristics of the dog named Bosco
+
+print(f"The name of the dog is: {bosco[0]}")
+print(f"The age of the dog is: {bosco[1]}")
+print(f"The breed of the dog is: {bosco[2]}")
+
+print()
+
+# display the characteristics of the dog named faith
+
+print(f"The name of the dog is: {faith[0]}")
+print(f"The age of the dog is: {faith[1]}")
+print(f"The breed of the dog is: {faith[2]}")
+
+print()
