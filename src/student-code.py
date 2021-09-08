1ca31454fab1d597f35d6e1f0963cdf86a481e3a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-09-08 13:17:12 -0400

    Add the student-code.py file in src.

---
+++ src/student-code.py
@@ -0,0 +1,15 @@
+
+def is_even(number):
+    return True if (number % 2 == 0) else False
+
+print(is_even(2))
+
+# def compute_mean(numbers_list : List):
+#     value = 0
+#     x = 0
+#     for len(numbers_list):
+#         value += numbers_list[x]
+#         x += 1
+#     length = len(numbers_list)
+#     mean = value/length
+#     return float(mean)
